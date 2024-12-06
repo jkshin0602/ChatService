b02c5b672bbefb287cca45c5e1ed806447ef350d
jkshin0602 <sjk6437@gmail.com>
2024-12-06 15:57:43 +0900

    feat: 로그인 API 추가

--- src/main/kotlin/com/chatservice/user_api/controller/UserController.kt
+++ src/main/kotlin/com/chatservice/user_api/controller/UserController.kt
@@ -2,8 +2,11 @@ package com.chatservice.user_api.controller
 
 import com.chatservice.common.response.ApiResult
 import com.chatservice.user_api.dto.request.JoinUserApiRequest
+import com.chatservice.user_api.dto.request.LoginUserApiRequest
 import com.chatservice.user_api.dto.response.JoinUserApiResponse
+import com.chatservice.user_api.dto.response.LoginUserApiResponse
 import com.chatservice.user_api.service.JoinUserService
+import com.chatservice.user_api.service.LoginUserService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,12 +16,21 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class UserController(
     private val joinUserService: JoinUserService,
+    private val loginUserService: LoginUserService,
 ) {
     @PostMapping
     fun join(
         @RequestBody request: JoinUserApiRequest,
     ): ApiResult<JoinUserApiResponse> {
         val result = joinUserService.join(request)
+        return ApiResult(data = result)
+    }
+
+    @PostMapping
+    fun login(
+        @RequestBody request: LoginUserApiRequest,
+    ): ApiResult<LoginUserApiResponse> {
+        val result = loginUserService.login(request)
         return ApiResult(data = result)
     }
 }
